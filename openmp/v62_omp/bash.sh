15eea5f3c62a5fcf5b504887caafe7fb010adca2
Lewis Sharpe <ls68@robotarium.cm.cluster>
2018-10-23 00:09:47 +0100

    pthread mod

---
+++ openmp/v62_omp/bash.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+# This script run the Minimax algorithm and print the resulting output to the file 'output.txt' .
+#
+#SBATCH --job-name=test_omp
+#SBATCH --output=ttt_omp.txt
+#
+#SBATCH --ntasks=8
+#SBATCH --cpus-per-task=4
+#SBATCH --time=10:00
+#SBATCH --mem-per-cpu=100
+
+export OMP_NUM_THREADS=$SLURM_CPUS_PER_TASK
+./ttt_omp
