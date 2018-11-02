6f3d50c5a1cbed356246193bdb9edd384cd915e6
Lewis Sharpe <ls68@robotarium.cm.cluster>
2018-11-02 15:00:47 +0000

    root commit

---
+++ ttt_new/new/seq/v4_seq_pm/bash.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+<<<<<<< HEAD
+# This script run the Minimax algorithm and print the resulting output to the file 'output.txt'.
+=======
+>>>>>>> 8bf6052e25bc27896d7e473a1260c348f17451d1
+#SBATCH --job-name=my_job
+#SBATCH --output=output.txt
+#
+#SBATCH --ntasks=1 --nodes=1
+#SBATCH --time=10:00
+#SBATCH --mem-per-cpu=6000
+
+./ttt
