322484fc76a6f998cf2020cab4243e63fafd7505
Lewis Sharpe <ls68@robotarium.cm.cluster>
2018-12-06 09:53:42 +0000

    twocount implemented, structural changes, prep for thres

---
+++ bash.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# This script run the Minimax algorithm and print the resulting output the file 'output.txt'.
+#SBATCH --job-name=my_job
+#SBATCH --output=output.txt
+#
+#SBATCH --ntasks=8 
+#SBATCH --cpus-per-task=4
+#SBATCH --time=10:00
+#SBATCH --mem-per-cpu=6000
+
+./ttt_pt_ver2
+<<<<<<< HEAD
+
+=======
+>>>>>>> 4725639e929f84e31d1236364ab89fb24e9ff000
